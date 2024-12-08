a24960413bb50b976735c94930f4fc36a773ee06
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 22:06:43 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'; //Es Module
 // const express = require('express') - common js
+import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.route.js'
 
-
+dotenv.config()
 
 const app = express();
+
+console.log("MONGO_URI: ", process.env.MONGO_URI);
 
 app.use('/api/v1/auth', authRoutes)
 
