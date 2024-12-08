f32698a6f63f5c35071852cb121b1db823ad8177
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 20:57:36 +0100

    Updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,2 +1,8 @@
-export async function signup (req, res){ 
+export async function signup(req, res){ 
     res.send('Signup route')}
+
+export async function login(req, res){ 
+    res.send('Login route')}
+
+export async function logout(req, res){ 
+    res.send('Login route')}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,9 @@
 import express from 'express'
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router =express.Router();
 
-router.get("/signup",  
-})
-router.get("/login", (req, res)=>{
-    res.send('Login route')
-})
-router.get("/logout", (req, res)=>{
-    res.send('Logout route')
-})
+router.get("/signup", signup)
+router.get("/login", login)
+router.get("/logout", logout)
 export default router;
