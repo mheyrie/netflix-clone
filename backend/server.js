6e8c200c663b8c395a27343d22bf062a4441b9ec
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 20:32:39 +0100

    Adding auth route

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ import express from 'express'; //Es Module
 
 const app = express();
 
-app.get("/signup", (req, res)=>{
+app.get("/signout", (req, res)=>{
     res.send('Signup route')
+})
+app.get("/login", (req, res)=>{
+    res.send('Login route')
+})
+app.get("/logout", (req, res)=>{
+    res.send('Logout route')
 })
 
 app.listen(5000, ()=>{
