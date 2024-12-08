0383d5ebc23f8b0dc499a770a3cfbe6390099431
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 19:43:46 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import express from 'express'; //Es Module
 
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send('Server is ready')
+})
+
 app.listen(5000, ()=>{
     console.log('Server started at http://localhost:5000');
 })
