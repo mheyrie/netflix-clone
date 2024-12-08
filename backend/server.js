3e88295eac69df9e40025e9d2372dc020a425722
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 20:39:02 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,7 @@ import express from 'express'; //Es Module
 
 const app = express();
 
-app.get("/api/v1/signup", (req, res)=>{
-    res.send('Signup route')
-})
-app.get("/api/v1/login", (req, res)=>{
-    res.send('Login route')
-})
-app.get("/api/v1/logout", (req, res)=>{
-    res.send('Logout route')
-})
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(5000, ()=>{
     console.log('Server started at http://localhost:5000');
