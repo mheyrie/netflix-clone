56b9ded703287e6206fb936c666829ea04d5d395
Mheyrie <omota4fidelity@gmail.com>
2024-12-08 19:39:56 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-const express = require('express')
+// const express = require('express') - common js
+import express from 'express'; //Es Module
+
+
+const app = express();
+
+app.listen(5000, ()=>{
+    console.log('Server started at http://localhost:5000');
+})
