c0f5a36eeaa86a1395f9e621beaed51c46db3059
Mheyrie <omota4fidelity@gmail.com>
2024-12-09 09:41:53 +0100

    sETTING UP PORT

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express"; //Es Module
 // const express = require('express') - common js
-
 import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
 });
