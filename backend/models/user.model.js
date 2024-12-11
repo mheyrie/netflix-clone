281c898f34db38fbb86539ad1c4f49bd48d4feee
Mheyrie <omota4fidelity@gmail.com>
2024-12-12 00:20:00 +0100

    Updating

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,10 +6,17 @@ const userSchema = mongoose.Schema({
     required: true,
     unique: true,
   },
-  email:{
-    type:String,
+  email: {
+    type: String,
     required: true,
     unique: true,
   },
-  
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: true,
+  },
 });
