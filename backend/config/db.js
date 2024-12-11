c3a877c9b31144d28a8efa0c7095fd7cf4a562c2
Mheyrie <omota4fidelity@gmail.com>
2024-12-11 23:50:01 +0100

    Setting up Mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ import { ENV_VARS } from "./envVars";
 export const connentDB = async()=>{
     try{
 const conn = await mongoose.connect(ENV_VARS.MONGO_URI)
+console.log("MongoDB connected: " + conn.connection.host);
     }catch(error){
 
     }
