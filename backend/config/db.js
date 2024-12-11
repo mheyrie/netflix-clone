e8b17925a74db0243664314988cf05316b332def
Mheyrie <omota4fidelity@gmail.com>
2024-12-11 23:48:53 +0100

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ import { ENV_VARS } from "./envVars";
 
 export const connentDB = async()=>{
     try{
-
+const conn = await mongoose.connect(ENV_VARS.MONGO_URI)
     }catch(error){
-        
+
     }
 }
