d8f4ea4e69b2e86597becb16f9212ab408b36f4c
Mheyrie <omota4fidelity@gmail.com>
2024-12-11 23:51:53 +0100

    Updating

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,6 @@ export const connentDB = async()=>{
 const conn = await mongoose.connect(ENV_VARS.MONGO_URI)
 console.log("MongoDB connected: " + conn.connection.host);
     }catch(error){
-
+process.exit(1)
     }
 }
