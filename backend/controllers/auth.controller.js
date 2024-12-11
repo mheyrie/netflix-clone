2b6b328753ceea11a40e42ed6bb304e937b6af84
Mheyrie <omota4fidelity@gmail.com>
2024-12-12 00:42:46 +0100

    Updated conditions for login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,9 +9,11 @@ export async function signup(req, res) {
 
     //check for actual email usage👇
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(!emailRegex.test(email)){
-        return res.status(400)
-        .json({ success: false, message: "Invalid Email" });
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+    if (password.length<6) {
+      return res.status(400).json({ success: false, message: "Password must be at least 6 characters" });
     }
   } catch (error) {}
 }
