54d60329ea65c7d5245d93d69ab6d6bd186b7191
Mheyrie <omota4fidelity@gmail.com>
2024-12-12 00:23:13 +0100

    Updating auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,15 @@
-export async function signup(req, res){ 
-    res.send('Signup route')}
+export async function signup(req, res) {
+try{
 
-export async function login(req, res){ 
-    res.send('Login route')}
+}catch(error){
+    
+}
+}
 
-export async function logout(req, res){ 
-    res.send('Logout route')}
+export async function login(req, res) {
+  res.send("Login route");
+}
+
+export async function logout(req, res) {
+  res.send("Logout route");
+}
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -26,4 +26,4 @@ const userSchema = mongoose.Schema({
 });
 
 
-const User = mongoose.model('User', userSchema)
+export const User = mongoose.model('User', userSchema)
