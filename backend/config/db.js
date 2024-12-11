f7d608e988cfdf0329fd45d8feb7bb46cabb886c
Mheyrie <omota4fidelity@gmail.com>
2024-12-12 00:00:54 +0100

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import { ENV_VARS } from "./envVars";
 
-
-export const connentDB = async()=>{
-    try{
-const conn = await mongoose.connect(ENV_VARS.MONGO_URI)
-console.log("MongoDB connected: " + conn.connection.host);
-    }catch(error){
-process.exit(1)
-    }
-}
+export const connentDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+    console.log("MongoDB connected: " + conn.connection.host);
+  } catch (error) {
+    console.error("Error connecting to MongoDB: " + error.message)
+    process.exit(1); //1 means there was an error, 0 means success
+  }
+};
