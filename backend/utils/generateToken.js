98bad8be5ea43abbb9c6cdd61f2b5fc4f228838a
Mheyrie <omota4fidelity@gmail.com>
2024-12-18 17:50:21 +0100

    working on token generation

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,6 +1,9 @@
-import jwt from "jsonwebtoken"
-import { ENV_VARS } from "../config/envVars"
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars";
 
-export const generateTokenAndSetCookie =(userId, res)=>{
-const token = jwt.sign({userId}, ENV_VARS.JWT_SECRET,{ expiresIn: "15d"})
-}
+export const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+  res.cookie("jwt-netflix", token, { 
+    maxAge: 15 * 24 * 60 * 60 * 1000 
+});
+};
