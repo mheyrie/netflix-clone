a0927e4db60ade9ee3ffb011a3a565a8be50b900
Mheyrie <omota4fidelity@gmail.com>
2024-12-19 00:40:14 +0100

    Updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,18 +67,20 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-  try{
-const {email, password}= req.body;
-if(!email || !password)
-  }catch(error){
-
-  }
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+  } catch (error) {}
 }
 
 export async function logout(req, res) {
   try {
-    res.clearCookie('jwt-netflix')
-    res.status(200).json({success: true, message: "Logged out successfully"})
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ success: false, message: "Internal server error" });
