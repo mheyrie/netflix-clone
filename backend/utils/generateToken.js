942e86408409ec0dac0df1e8bca6b30e5c8acbb1
Mheyrie <omota4fidelity@gmail.com>
2024-12-18 18:28:46 +0100

    Updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,6 +4,8 @@ import { ENV_VARS } from "../config/envVars";
 export const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
   res.cookie("jwt-netflix", token, { 
-    maxAge: 15 * 24 * 60 * 60 * 1000, //15 days in Millisecond
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in Millisecond
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not by JS
+    sameSite: "strict",
 });
 };
