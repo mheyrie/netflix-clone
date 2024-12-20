b06cf1813a3e819f27e452222c2ebaf4fd428c25
Mheyrie <omota4fidelity@gmail.com>
2024-12-20 18:38:06 +0100

    Updated

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,3 +1,16 @@
 import axios from 'axios'
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjQ3ZDFhNzAwNWQ4Yjc2NGI0NjE5YWIxYmU3YTVhMyIsIm5iZiI6MTczNDcyNTk4MS43Niwic3ViIjoiNjc2NWQxNWRhZmY1YTdkNjQxNzRiMWQ3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.CVCbknyeLGk4Otyhj6rjVlNOkJysqHU7hCHwlg0acwg'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
 export const fetchFromTMDB = async (url)=>{}
