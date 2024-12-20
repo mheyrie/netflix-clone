e2830f8dec0c6045212c21b4016e524f11a26254
Mheyrie <omota4fidelity@gmail.com>
2024-12-20 18:19:25 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-const PORT = ENV_VARS.PORT
+const PORT = ENV_VARS.PORT;
 
-app.use(express.json()) //Allow use to use/parse req.body
+app.use(express.json()); //Allow use to use/parse req.body
 
 app.use("/api/v1/auth", authRoutes);
 
@@ -16,18 +16,18 @@ app.listen(PORT, () => {
   connectDB();
 });
 
-
-
-
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer '
-  }
+    accept: "application/json",
+    Authorization: "Bearer "+ ENV_VARS.TMDB_API_KEY, 
+  },
 };
 
-fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+fetch(
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+  options
+)
+  .then((res) => res.json())
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
