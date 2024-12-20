8302c62da419ef8830c4ab9f99c4e47cd2a032ea
Mheyrie <omota4fidelity@gmail.com>
2024-12-20 17:48:53 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjQ3ZDFhNzAwNWQ4Yjc2NGI0NjE5YWIxYmU3YTVhMyIsIm5iZiI6MTczNDcyNTk4MS43Niwic3ViIjoiNjc2NWQxNWRhZmY1YTdkNjQxNzRiMWQ3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.CVCbknyeLGk4Otyhj6rjVlNOkJysqHU7hCHwlg0acwg'
+    Authorization: 'Bearer '
   }
 };
 
