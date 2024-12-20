553e67445accb8ffa29044d2e4f3eddc5b12668a
Mheyrie <omota4fidelity@gmail.com>
2024-12-20 18:22:47 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,3 @@ app.listen(PORT, () => {
   connectDB();
 });
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: "Bearer "+ ENV_VARS.TMDB_API_KEY, 
-  },
-};
-
-fetch(
-  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-  options
-)
-  .then((res) => res.json())
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
