1477d8bf9018282ee71eaaed5fda3dfd0d05035b
Mheyrie <omota4fidelity@gmail.com>
2024-12-20 18:42:18 +0100

    Updated

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { ENV_VARS } from "../config/envVars";
 
-fetch(
-  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-  options
-)
-  .then((res) => res.json())
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+// fetch(
+//   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+//   options
+// )
+//   .then((res) => res.json())
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
 
 export const fetchFromTMDB = async (url) => {
   const options = {
@@ -17,4 +17,5 @@ export const fetchFromTMDB = async (url) => {
       Authorization: "Bearer " + ENV_VARS.TMDB_API_KEY,
     },
   };
+  const response = await axios.get(url, options)
 };
