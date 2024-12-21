e2c855cc49f4f028274bd06e2375f702a8f79e37
Mheyrie <omota4fidelity@gmail.com>
2024-12-21 23:37:39 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //Es Module
 // const express = require('express') - common js
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -10,7 +11,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); //Allow use to use/parse req.body
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
