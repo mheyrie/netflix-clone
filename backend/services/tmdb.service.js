6e6e5425bf80a2c8a09d4bf04d2f98945e69b142
Mheyrie <omota4fidelity@gmail.com>
2024-12-21 23:35:38 +0100

    updated

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -19,7 +19,7 @@ export const fetchFromTMDB = async (url) => {
   };
   const response = await axios.get(url, options)
   if(response.status !== 200){
-    throw new Error
+    throw new Error('ailed to fetch data from TMDB' )
   }
   return response.data
 };
