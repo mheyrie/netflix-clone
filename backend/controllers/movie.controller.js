9d20ed37100df601818d4f742ce4ce5790cd0e01
Mheyrie <omota4fidelity@gmail.com>
2024-12-22 02:35:46 +0100

    Updating fetch url

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,8 +1,8 @@
-import { fetchFromTMDB } from "../services/tmdb.service";
+import { fetchFromTMDB } from "../services/tmdb.service.js";
 
 export async function getTrendingMovie(req, res){
     try{
-const data = await fetchFromTMDB
+const data = await fetchFromTMDB()
     }catch(error){
 
     }
