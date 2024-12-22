ab0430e361198c526e625bd17c5b59f1872c91c1
Mheyrie <omota4fidelity@gmail.com>
2024-12-22 02:26:36 +0100

    Updated

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,7 +1,9 @@
+import { fetchFromTMDB } from "../services/tmdb.service";
+
 export async function getTrendingMovie(req, res){
     try{
-
+const data = await fetchFromTMDB
     }catch(error){
-        
+
     }
 }
