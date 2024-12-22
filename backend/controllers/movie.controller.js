046ce429291b0e4d99a64f616b4bfefcd0c63d5b
Mheyrie <omota4fidelity@gmail.com>
2024-12-22 02:42:05 +0100

    Updated

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -5,6 +5,9 @@ export async function getTrendingMovie(req, res) {
     const data = await fetchFromTMDB(
       "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
     );
-    const randomMovie = data.results(Math.floor(Math.random()* data.results?.length))
+    const randomMovie = data.results(
+      Math.floor(Math.random() * data.results?.length)
+    );
+    res.json({success:true, content:randomMovie})
   } catch (error) {}
 }
