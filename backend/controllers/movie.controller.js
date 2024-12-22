c7bd9b1bbb550f110a88e0084f7a0a65bfdd035e
Mheyrie <omota4fidelity@gmail.com>
2024-12-22 02:38:02 +0100

    Updating

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,9 +1,10 @@
 import { fetchFromTMDB } from "../services/tmdb.service.js";
 
-export async function getTrendingMovie(req, res){
-    try{
-const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
-    }catch(error){
-
-    }
+export async function getTrendingMovie(req, res) {
+  try {
+    const data = await fetchFromTMDB(
+      "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+    );
+    data.results(Math.floor(Math.random()))
+  } catch (error) {}
 }
