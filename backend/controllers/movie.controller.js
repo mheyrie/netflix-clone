9992604f5125a42bd2557d315431bd061cc0b9ee
Mheyrie <omota4fidelity@gmail.com>
2024-12-22 02:36:33 +0100

    Updated

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -2,7 +2,7 @@ import { fetchFromTMDB } from "../services/tmdb.service.js";
 
 export async function getTrendingMovie(req, res){
     try{
-const data = await fetchFromTMDB()
+const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
     }catch(error){
 
     }
