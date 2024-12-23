18885951fd6aeee7d4353efc392e722158a2d513
Mheyrie <omota4fidelity@gmail.com>
2024-12-24 00:32:35 +0100

    added movies category

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -37,22 +37,32 @@ export async function getMovieDetails(req, res) {
     res.status(200).json({ success: true, content: data });
   } catch (err) {
     if (err.message.includes(" 404")) {
-        return res.status(404).send(null);
-      }
-      res.status(500).json({ success: false, message: "Internal Server error" });
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: "Internal Server error" });
   }
 }
+
 export async function getSimilarMovies(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
       `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
     );
-    res.status(200).json({ success: true, content: data });
+    res.status(200).json({ success: true, similar: data.results });
   } catch (err) {
-    if (err.message.includes(" 404")) {
-        return res.status(404).send(null);
-      }
-      res.status(500).json({ success: false, message: "Internal Server error" });
+    res.status(500).json({ success: false, message: "Internal Server error" });
+  }
+}
+
+export async function getMoviesByCategory(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+    res.status(200).json({ success: true, similar: data.results });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal Server error" });
   }
 }
