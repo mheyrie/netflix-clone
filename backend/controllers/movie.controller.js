8aa8c879e72ccbaaaca766cbfdde2adb98fb89e5
Mheyrie <omota4fidelity@gmail.com>
2024-12-23 23:44:17 +0100

    Updated trailer function

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -16,12 +16,14 @@ export async function getTrendingMovie(req, res) {
 export async function getMovieTrailers(req, res) {
   const { id } = req.params;
   try {
-    const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`)
-    res.json({success: true, trailers:data.results})
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    res.json({ success: true, trailers: data.results });
   } catch (err) {
-    if(err.message.includes(' 404')){
-        return res.status(404).send(null)
+    if (err.message.includes(" 404")) {
+      return res.status(404).send(null);
     }
-    res.status(500).json({success: false, message:'Internal Server error'})
+    res.status(500).json({ success: false, message: "Internal Server error" });
   }
 }
