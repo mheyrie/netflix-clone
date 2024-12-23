ae04a5907a230c74750023f850576067694d0e75
Mheyrie <omota4fidelity@gmail.com>
2024-12-23 23:01:08 +0100

    bug fix

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -5,9 +5,9 @@ export async function getTrendingMovie(req, res) {
     const data = await fetchFromTMDB(
       "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
     );
-    const randomMovie = data.results(
+    const randomMovie = data.results[
       Math.floor(Math.random() * data.results?.length)
-    );
+    ];
     res.json({ success: true, content: randomMovie });
   } catch (error) {
     res.status(500).json({ success: false, message: " Internal Server Error" });
