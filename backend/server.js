a158026dbfaa7f4f670d2a48f6545cb37c87d769
Mheyrie <omota4fidelity@gmail.com>
2024-12-24 01:00:36 +0100

    Updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"; //Es Module
 // const express = require('express') - common js
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
