a2d5260facee5be9f61cc3f66d8b65aa7cb5cd95
mheyrie <omota4fidelity@gmail.com>
2024-12-27 10:20:57 +0000

    protectRoute

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import { User } from "../models/user.model.js"
 import { ENV_VARS } from "../config/envVars.js"
 
 
-export default protectRoute = async (req, res, next) => {
+export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies['jwt-netflix']
         if (!token) {
@@ -17,8 +17,11 @@ export default protectRoute = async (req, res, next) => {
         if (!user) {
             return res.status(401).json({ success: false, message: "User not found" })
         }
+
+        req.user = user;
         next()
     } catch (err) {
-
+        console.log(" Error in protectedRoute middleware: ", err.message)
+        res.status(500).json({ success: false, message: ' Internal Server error' });
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+
 
 
 const app = express();
@@ -18,7 +20,7 @@ app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
-app.use("/api/v1/tv",protectRoute, tvRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
