41e77974bff55d0f246b1c0b3c9eca26476bc392
mheyrie <omota4fidelity@gmail.com>
2024-12-27 10:15:54 +0000

    updated

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -6,13 +6,18 @@ import { ENV_VARS } from "../config/envVars.js"
 export default protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies['jwt-netflix']
-        if(!token){
-            return res.status(401).json({success: false, message: "Unauthorized -  No Token Provided"})
+        if (!token) {
+            return res.status(401).json({ success: false, message: "Unauthorized -  No Token Provided" })
         }
         const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
-        if(!decoded){
-            return res.status(401).json({success: false, message: "Invalid Token"})
+        if (!decoded) {
+            return res.status(401).json({ success: false, message: "Invalid Token" })
         }
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) {
+            return res.status(401).json({ success: false, message: "User not found" })
+        }
+        next()
     } catch (err) {
 
     }
