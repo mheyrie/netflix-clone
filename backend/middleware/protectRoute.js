e0e1120baa2ccc426e261ea65c07dc783bd2c072
mheyrie <omota4fidelity@gmail.com>
2024-12-27 10:11:03 +0000

    Updating

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,10 +3,13 @@ import { User } from "../models/user.model.js"
 import { ENV_VARS } from "../config/envVars.js"
 
 
-export default protectRoute = async (req, res, next )=>{
-    try{
-const token = req.cookies['jwt-netflix']
-    } catch (err){
+export default protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies['jwt-netflix']
+        if(!token){
+            return res.status(401).json({success: false, message: "Unauthorized -  No Token Provided"})
+        }
+    } catch (err) {
 
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express"; //Es Module
 // const express = require('express') - common js
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //Allow use to use/parse req.body
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
