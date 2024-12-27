6a99a142bbbc5db55f9eee53aaeae9e633508044
mheyrie <omota4fidelity@gmail.com>
2024-12-27 09:40:04 +0000

    api updates

--- backend/controllers/tv.controller.js
+++ backend/controllers/tv.controller.js
@@ -3,7 +3,7 @@ import { fetchFromTMDB } from "../services/tmdb.service.js";
 export async function getTrendingTv(req, res) {
   try {
     const data = await fetchFromTMDB(
-      "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+      "https://api.themoviedb.org/3/trending/tv/day?language=en-US"
     );
     const randomMovie =
       data.results[Math.floor(Math.random() * data.results?.length)];
@@ -17,7 +17,7 @@ export async function getTvTrailers(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`
     );
     res.json({ success: true, trailers: data.results });
   } catch (err) {
@@ -32,7 +32,7 @@ export async function getTvDetails(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US`
     );
     res.status(200).json({ success: true, content: data });
   } catch (err) {
@@ -47,7 +47,7 @@ export async function getSimilarTvs(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/${id}/similar?language=en-US&page=1`
     );
     res.status(200).json({ success: true, similar: data.results });
   } catch (err) {
