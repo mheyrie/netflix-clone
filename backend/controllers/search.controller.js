09816ee7da3a1aeed112e8cbd1d2ef6c769dbd40
mheyrie <omota4fidelity@gmail.com>
2024-12-28 19:39:40 +0000

    dynamic search from person

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,7 +1,9 @@
 export async function searchPerson(req, res) {
-  const query = req.params.query;
-  const results = await searchPersonQuery(query);
-  res.json(results);
+    // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
+    const { query } = req.params;
+  try{
+    const response = await fetch(  `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+  }catch(err){console.log(err)}
 }
 
 export async function searchMovie(req, res) {
--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { searchMovie, searchPerson, searchTv } from "../controllers/search.controller";
+import { searchMovie, searchPerson, searchTv } from "../controllers/search.controller.js";
 
 const router = express.Router();
 
