f3a50d6c70710a2583a63d30769c4d36362cf4e9
mheyrie <omota4fidelity@gmail.com>
2024-12-28 19:40:57 +0000

    updated

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -2,7 +2,10 @@ export async function searchPerson(req, res) {
     // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
     const { query } = req.params;
   try{
-    const response = await fetch(  `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+    const response = await fetch(  `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+    if(response.results.length === 0){
+      res.status(404).send(null)
+    }
   }catch(err){console.log(err)}
 }
 
