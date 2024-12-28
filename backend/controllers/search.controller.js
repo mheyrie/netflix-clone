bf4ad6c9ca1d6c093c145a0fa6e7621de22a5967
mheyrie <omota4fidelity@gmail.com>
2024-12-28 20:15:03 +0000

    Updated

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,11 +1,15 @@
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+import{User} from "../models/user.model.js";
+
+
 export async function searchPerson(req, res) {
-    // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
     const { query } = req.params;
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
         if (response.results.length === 0) {
             res.status(404).send(null)
         }
+        User
         res.status(200).jsonn({ success: true, content: response.results })
     } catch (err) { console.log("Error in searchPerson controller: ", err.message) ;
         res.status(500).send({ success: false, message: "Internal server error" });
