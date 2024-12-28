b12df82a472f8b12eaf4055e22c8cbeb1a52175c
mheyrie <omota4fidelity@gmail.com>
2024-12-28 19:59:05 +0000

    updated

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,23 +1,23 @@
 export async function searchPerson(req, res) {
     // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
     const { query } = req.params;
-  try{
-    const response = await fetch(  `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
-    if(response.results.length === 0){
-      res.status(404).send(null)
-    }
-res.status(200).jsonn({success:true, content:response.results})
-  }catch(err){console.log(err)}
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        if (response.results.length === 0) {
+            res.status(404).send(null)
+        }
+        res.status(200).jsonn({ success: true, content: response.results })
+    } catch (err) { console.log(err) }
 }
 
 export async function searchMovie(req, res) {
-  const query = req.params.query;
-  const results = await searchMovieQuery(query);
-  res.json(results);
+    const query = req.params.query;
+    const results = await searchMovieQuery(query);
+    res.json(results);
 }
 
 export async function searchTv(req, res) {
-  const query = req.params.query;
-  const results = await searchTvQuery(query);
-  res.json(results);
+    const query = req.params.query;
+    const results = await searchTvQuery(query);
+    res.json(results);
 }
