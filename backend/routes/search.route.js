72c24366101d8015c5ae40e4b5e7f61eec2267cf
mheyrie <omota4fidelity@gmail.com>
2024-12-29 16:49:38 +0000

    Adding search history

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { searchMovie, searchPerson, searchTv } from "../controllers/search.controller.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
 router.get("/tv/:query", searchTv);
+
+router.get("/history", getSearchHistory);
 
 
 export default router;
