8838ea466ff0db200016faa274d2c0b1e263ac86
mheyrie <omota4fidelity@gmail.com>
2024-12-29 17:12:24 +0000

    updated

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -68,7 +68,7 @@ export async function searchTv(req, res) {
 
 export async function getSearchHistory(req, res){
     try{
-res.status(200).json
+res.status(200).json({success: true, content: req.user.searchHistory});
     }catch(err){
                 res.status(500).send({success: false, message: "Internal server error"});
     }
