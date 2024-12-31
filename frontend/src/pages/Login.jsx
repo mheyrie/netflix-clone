8544c3ff144485a24d68400aa646d361886a5960
Mheyrie <omota4fidelity@gmail.com>
2024-12-31 16:06:08 +0100

    unescape entittes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,72 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin=(e) =>{
+e.preventDefault();
+  }
   return (
-    <div>Page</div>
-  )
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+        <Link to={"/login"}>
+          <button className="bg-red-600 text-white font-bold p-2 px-3 rounded-md">
+            Sign In
+          </button>
+        </Link>
+      </header>
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full p-8 space-y-4 bg-black/60 rounded-lg shadow-md mx-auto">
+         
+          <h1 className="text-white text-center text-xl font-bold">Login In</h1>
+          <form onSubmit={handleLogin} className="space-y-4 max-w-md mx-auto">
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="youremail@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+           
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="*********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700" >Sign Up</button>
+          </form>
+          <div className="text-center text-gray-400">
+           Don"t have an account?{" "}
+            <Link to="/signup" className="text-red-600 hover:underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
