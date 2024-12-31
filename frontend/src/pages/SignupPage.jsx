fb0a57ad52e2b9b3941238edd5a4c46ce28ab543
Mheyrie <omota4fidelity@gmail.com>
2024-12-31 13:07:41 +0100

    Setting up signup page

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function SignupPage() {
   return (
-    <div>SignupPage</div>
+    <div className="h-screen w-full hero-bg">
+        <header className="max-w-6xl max-auto flex items-center justify-between p-4">
+            <Link to={'/'}>
+            <img src="/netflix-logo.png" alt="logo" className="w-52" /></Link>
+        </header>
+    </div>
   )
 }
