7e3d5ae08705b09e9ed905e2a592f16d73af4e80
Mheyrie <omota4fidelity@gmail.com>
2024-12-31 01:25:18 +0100

    added routes and image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import Login from "./pages/Login";
+
 
 
 function App() {
   return (
    <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/about" element={<About />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignupPage />} />
       </Routes>
   );
 }
