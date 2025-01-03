60d7dc3c29b7976ea22f4e8dcf0325fe77b4ba26
Mheyrie <omota4fidelity@gmail.com>
2025-01-03 23:21:21 +0100

    Udaetd

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
+        <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+<div className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left"></div>
+        </div>
+    </footer>
   )
 }
