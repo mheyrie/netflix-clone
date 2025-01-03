2537ae6626ceed61212e091d56024c0e54aa3afd
Mheyrie <omota4fidelity@gmail.com>
2025-01-03 23:05:02 +0100

    Updated

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -168,11 +168,16 @@ export default function AuthScreen() {
               Watch everywhere
             </h2>
             <p className="text-lg md:text-xl">
-             Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV without paying more.
             </p>
           </div>
           <div className="flex-1 relative overflow-hidden">
-            <img src="/device-pile.png" alt="device image" className="mt-4 z-20 relative" />
+            <img
+              src="/device-pile.png"
+              alt="device image"
+              className="mt-4 z-20 relative"
+            />
             <video
               className="absolute top-2 left-1/2 -translate-x-1/2  h-4/6 z-10 max-w-[63%]"
               playsInline
@@ -191,17 +196,21 @@ export default function AuthScreen() {
       {/* fourth section  */}
       <div className="py-10 bg-black text-white">
         <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
+          <div className="flex-1 relative overflow-hidden">
+            <img
+              src="/.png"
+              alt="device image"
+              className="mt-4 z-20 relative"
+            />
+          </div>
           <div className="flex-1 text-center md:text-left">
             <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
               Watch everywhere
             </h2>
             <p className="text-lg md:text-xl">
-             Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV without paying more.
             </p>
-          </div>
-          <div className="flex-1 relative overflow-hidden">
-            <img src="/device-pile.png" alt="device image" className="mt-4 z-20 relative" />
-          
           </div>
         </div>
       </div>
