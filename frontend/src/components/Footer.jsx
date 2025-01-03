387778cb065445cfc1edaa5908a55772da3d4e2d
Mheyrie <omota4fidelity@gmail.com>
2025-01-03 23:23:59 +0100

    Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
         <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-<div className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left"></div>
+<p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+    Built by <a href="https://www.linkedin.com/in/oladele-mary/" target="_blank" className="font-meium"
+</p>
         </div>
     </footer>
   )
