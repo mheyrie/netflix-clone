1eb7dcb809c0d3597097de6a8510f4e1adb77141
Mheyrie <omota4fidelity@gmail.com>
2025-01-09 23:55:53 +0100

    Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,26 +3,90 @@ export default function Footer() {
     <footer className="py-20 bg-black text-white flex justify-center">
       <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="text-center md:text-left">
-          <p className="text-sm leading-loose text-muted-foreground">
-            Questions? Call 1-800-000-0000
-          </p>
-          <ul className="grid grid-cols-2 md:grid-cols-4 justify-center md:justify-start gap-4 my-4">
-            <li><a href="#" className="text-sm text-muted-foreground">FAQ</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Help Center</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Account</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Media Center</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Investor Relations</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Jobs</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Ways to Watch</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Terms of Use</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Privacy</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Cookie Preferences</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Corporate Information</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Contact Us</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Speed Test</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Legal Notices</a></li>
-            <li><a href="#" className="text-sm text-muted-foreground">Netflix Originals</a></li>
-          </ul>
+          <div>
+            <p className="text-sm leading-loose text-muted-foreground">
+              Questions? Call 1-800-000-0000
+            </p>
+          </div>
+          <div>
+            <ul className="grid grid-cols-2 md:grid-cols-4 justify-center md:justify-start gap-4 my-4">
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  FAQ
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Help Center
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Account
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Media Center
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Investor Relations
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Jobs
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Ways to Watch
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Terms of Use
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Cookie Preferences
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Corporate Information
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Speed Test
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Legal Notices
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-sm text-muted-foreground">
+                  Netflix Originals
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="mt-4 md:mt-0">
           <p className="text-sm text-muted-foreground">© 2023 Netflix, Inc.</p>
