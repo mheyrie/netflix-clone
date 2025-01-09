08bc562b648c65af8355e351b74daf7b658fb0c2
Mheyrie <omota4fidelity@gmail.com>
2025-01-09 23:48:46 +0100

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/SignupPage";
 import Login from "./pages/Login";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
        <Route path="/login" element={<Login />} />
        <Route path="/signup" element={<SignupPage />} />
      </Routes>
-     <Footer/>
+     {/* <Footer/> */}
      
    </>
   );
