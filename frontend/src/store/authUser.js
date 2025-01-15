83a76ae01dd10bcea5e5dff6055977c73bebb91e
Mheyrie <omota4fidelity@gmail.com>
2025-01-16 00:47:38 +0100

    Updated

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -3,32 +3,31 @@ import toast from "react-hot-toast";
 import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
-user:null,
-isSigningUp:false,
-isCheckingAuth:true,
-signup: async(credentials)=>{
-    set
-    try{
-        const response = await axios.post("/api/v1/auth/signup",credentials);
-        set({user:response.data.user, isSigningUp:false})
-        toast.success("Account created successfully")
-    }catch(error){
-        toast.error(error.response.data.message || "An error occurred") 
-        set({isSigningUp:false, user:null})
-        console.log(error)
+  user: null,
+  isSigningUp: false,
+  isCheckingAuth: true,
+  signup: async (credentials) => {
+    set;
+    try {
+      const response = await axios.post("/api/v1/auth/signup", credentials);
+      set({ user: response.data.user, isSigningUp: false });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "An error occurred");
+      set({ isSigningUp: false, user: null });
+      console.log(error);
     }
-},
-login: async()=>{},
-logout: async()=>{},
-authCheck: async()=>{
-    set({isCheckingAuth:true})
-    try{
-const response = await axios.get("/api/v1/auth/authCheck");
-set({user:response.data.user, isCheckingAuth:false})
+  },
+  login: async () => {},
+  logout: async () => {},
+  authCheck: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const response = await axios.get("/api/v1/auth/authCheck");
+      set({ user: response.data.user, isCheckingAuth: false });
+    } catch (error) {
+      set({ isCheckingAuth: false, user: null });
+      console.log("Error", error);
     }
-    catch(error){
-        set({isCheckingAuth:false, user:null})
-console.log("Error", error)
-    }
-},
-}))
+  },
+}));
