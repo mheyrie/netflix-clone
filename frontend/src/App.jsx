6b3ff0159b617eb88e73f96d2f51f4497f7735e4
Mheyrie <omota4fidelity@gmail.com>
2025-01-18 00:00:05 +0100

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import SignupPage from "./pages/SignupPage";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
-// import Footer from "./components/Footer";
+
 
 function App() {
   const {user, isCheckingAuth} = useAuthStore();
