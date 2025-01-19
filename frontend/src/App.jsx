8f430952bd9c2efab641d9ad3faffcd2befab112
Mheyrie <omota4fidelity@gmail.com>
2025-01-19 07:44:19 +0100

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import SignupPage from "./pages/SignupPage";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
+import { use } from "react";
+import { useEffect } from "react";
 
 
 function App() {
   const {user, isCheckingAuth, authCheck} = useAuthStore();
   console.log('auth user is here:', user);
+
+  useEffect(() => {authCheck();}, []);
   return (
    <>
      <Routes>
