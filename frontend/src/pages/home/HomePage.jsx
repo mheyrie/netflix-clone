f117379465d7e51041a56021773b66b26041f396
Mheyrie <omota4fidelity@gmail.com>
2025-01-20 23:22:53 +0100

    Updated

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,7 +1,9 @@
+import { use } from "react";
 import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
+import { useAuthStore } from "../../store/authUser";
 
 export default function HomePage() {
-  const {user} = false;
+  const {user} = useAuthStore();
   return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
 }
