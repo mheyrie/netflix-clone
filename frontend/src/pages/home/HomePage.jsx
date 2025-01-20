573d993a52cb2c55c9ff46e9fef7171dd3a1d172
Mheyrie <omota4fidelity@gmail.com>
2025-01-20 23:22:24 +0100

    updated

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -2,6 +2,6 @@ import AuthScreen from "./AuthScreen";
 import HomeScreen from "./HomeScreen";
 
 export default function HomePage() {
-  const user = false;
+  const {user} = false;
   return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
 }
