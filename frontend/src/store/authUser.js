5414b512c62ec74abf5d72e1d81b482d05822323
Mheyrie <omota4fidelity@gmail.com>
2025-01-22 20:17:51 +0100

    bug fixed

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -21,8 +21,9 @@ export const useAuthStore = create((set) => ({
   },
   login: async () => {},
   logout: async () => {
+    set({ isLoggingOut: true });
     try{
-      await axios.get("/api/v1/auth/logout");
+      await axios.post("/api/v1/auth/logout");
       set({ user: null, isLoggingOut:false });
       toast.success("Logged out successfully")
     }catch(error){
