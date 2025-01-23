9e59c6285132495827c0bf194cc9fbba789aa7d6
Mheyrie <omota4fidelity@gmail.com>
2025-01-23 01:27:22 +0100

    updated

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -18,12 +18,12 @@ export default function HomeScreen() {
           aria-hidden="true"
         />
 
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32" />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32" >
 
         <div className="bg-gradient-to-b from-gray-800 via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
 
         <div className="max-w-2xl">
-          <h1 className="mt-4 text-6xl font-extrabold">Extraction</h1>
+          <h1 className="mt-4 text-6xl font-extrabold text-balance">Extraction</h1>
           <p className="mt-2 text-lg">2014 | 18+</p>
           <p className="mt-4 text-lg">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
@@ -37,7 +37,7 @@ export default function HomeScreen() {
           <Play className="size-6 inline-block mr-2 fill-black"/>
           Play
           </Link>
-        </div>
+        </div></div>
       </div>
     </>
   );
