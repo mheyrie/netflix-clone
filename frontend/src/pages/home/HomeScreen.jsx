d8bf49cdde3fb106af0c703d152be49acd623c42
Mheyrie <omota4fidelity@gmail.com>
2025-01-23 01:33:33 +0100

    updated

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import { Play } from "lucide-react";
+import { Info, Play } from "lucide-react";
 
 export default function HomeScreen() {
   return (
@@ -18,26 +18,38 @@ export default function HomeScreen() {
           aria-hidden="true"
         />
 
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32" >
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+          <div className="bg-gradient-to-b from-gray-800 via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
 
-        <div className="bg-gradient-to-b from-gray-800 via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
-
-        <div className="max-w-2xl">
-          <h1 className="mt-4 text-6xl font-extrabold text-balance">Extraction</h1>
-          <p className="mt-2 text-lg">2014 | 18+</p>
-          <p className="mt-4 text-lg">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
-            dolores doloremque nobis excepturi voluptate quo nam alias
-            consectetur dicta, ad sapiente soluta, provident nulla eaque
-            voluptas totam earum tempore mollitia!
-          </p>
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold text-balance">
+              Extraction
+            </h1>
+            <p className="mt-2 text-lg">2014 | 18+</p>
+            <p className="mt-4 text-lg">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus
+              dolores doloremque nobis excepturi voluptate quo nam alias
+              consectetur dicta, ad sapiente soluta, provident nulla eaque
+              voluptas totam earum tempore mollitia!
+            </p>
+          </div>
+          <div className="flex mt-8">
+            <Link
+              to="/watch/123"
+              className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded pr-4 flex items-center"
+            >
+              <Play className="size-6 mr-2 fill-black" />
+              Play
+            </Link>
+            <Link
+              to="/watch/123"
+              className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center ml-4"
+            >
+              <Info className="size-6 mr-2 " />
+             More Info
+            </Link>
+          </div>
         </div>
-        <div className="flex mt-8">
-          <Link to="/watch/123" className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded pr-4 flex items-center">
-          <Play className="size-6 inline-block mr-2 fill-black"/>
-          Play
-          </Link>
-        </div></div>
       </div>
     </>
   );
