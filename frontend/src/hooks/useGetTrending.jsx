c2f8d1408fc52f4603cb6c01049e4f034644c36f
Mheyrie <omota4fidelity@gmail.com>
2025-02-17 00:27:39 +0100

    usehook for content

--- frontend/src/hooks/useGetTrending.jsx
+++ frontend/src/hooks/useGetTrending.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import { useContentStore } from "../store/content";
 import { useEffect } from "react";
 import axios from "axios";
+import { response } from "express";
 
 const useGetTrending = () => {
   const [trendingContent, setTrendingContent] = useState(null);
-  const {contentType}=useContentStore();
+  const { contentType } = useContentStore();
 
-  useEffect(()=>{
-    const getTrendingContent=async()=>{
-        await axios.get(`/api/v1/content/trending/${contentType}`)
-    }
-  })
-}
-export default useGetTrending
+  useEffect(() => {
+    const getTrendingContent = async () => {
+      await axios.get(`/api/v1/${contentType}/trending`);
+      setTrendingContent(response.data.content);
+      
+    };getTrendingContent()
+  }, [contentType]);
+  return {trendingContent}
+};
+export default useGetTrending;
