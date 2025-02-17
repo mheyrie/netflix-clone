aa100ba5f077406fd7bc78fa2908756b19ed8d5f
Mheyrie <omota4fidelity@gmail.com>
2025-02-17 03:31:06 +0100

    implenting categories page

--- frontend/src/store/content.js
+++ frontend/src/store/content.js
@@ -4,3 +4,7 @@ export const useContentStore = create((set) => ({
   contentType: "movie",
   setContentType: (type) => set({ contentType: type }),
 }));
+
+
+export const MOVIE_CATEGORIES = ["now_playing", "popular", "top_rated", "upcoming"];
+export const TV_CATEGORIES = ["airing_today", "on_the_air", "popular", "top_rated"];
