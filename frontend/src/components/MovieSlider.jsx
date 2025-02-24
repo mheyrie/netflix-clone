13fe66b5078e3051956eeb1b529345608e616fa8
Mheyrie <omota4fidelity@gmail.com>
2025-02-24 09:53:10 +0100

    updating MovieSlider

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types';
+import { useContentStore } from "../store/content";
 
 export const MovieSlider = ({category}) => {
     console.log("category is here:", category);
+    const {contentType} = useContentStore();
+    const formattedContentType = contentType === "movie"?"Movies":"TV Shows"
   return (
-    <div className="text-white">{category}</div>
+    <div className="text-white bg-black relative px-5 md:px-20">
+      <h2 className="">
+        {category} {formattedContentType}
+      </h2>
+      {category}</div>
   )
 }
 
