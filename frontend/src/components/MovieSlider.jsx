1fcebc01e3b67ce1453c2480bdad87377ca2d997
Mheyrie <omota4fidelity@gmail.com>
2025-02-25 09:44:48 +0100

    Updated

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -27,14 +27,17 @@ export const MovieSlider = ({ category }) => {
       <h2 className="">
         {formattedCategoryName} {formattedContentType}
       </h2>
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 overflow-x-scroll">
         {content.map((item)=>(
           <Link to={`/watch/${item.id}`} className="min-w-[250px] relative group" key={item.id}>
             <div className="rounded-lg overflow-hidden">
               <img src={SMALL_IMG_BASE_URL+item.backdrop_path} alt=""
-              className="transition-transform duration-300 ease-in-out group-hover:scale-105"
+              className="transition-transform duration-300 ease-in-out group-hover:scale-125"
               />
             </div>
+            <p className="mt-2 text-center">
+              {item.title || item.name}
+            </p>
           </Link>
         )
         )}
