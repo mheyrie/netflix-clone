9c1aef9fcd9093eb223ff88bd4855b9bb02b504a
Mheyrie <omota4fidelity@gmail.com>
2025-02-25 09:21:58 +0100

    updating get for movie slider

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,18 +1,33 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { useContentStore } from "../store/content";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export const MovieSlider = ({category}) => {
-    console.log("category is here:", category);
-    const {contentType} = useContentStore();
-    const formattedContentType = contentType === "movie"?"Movies":"TV Shows"
+
+export const MovieSlider = ({ category }) => {
+  const { contentType } = useContentStore();
+  const [content, setContent] = useState([]);
+
+  console.log("category is here:", category);
+
+  const formattedContentType = contentType === "movie" ? "Movies" : "TV Shows";
+ const formattedCategoryName = category.replaceAll("_"," ")[0].toUpperCase() + category.replaceAll("_"," ").slice(1);
+
+ useEffect(()=>{
+  const getContent = async()=>{
+    const res = await axios.get(`/api/v1/${contentType}/${category}`);
+    setContent(res.data.content);
+  }
+  getContent()
+ }, [category, contentType]);
   return (
     <div className="text-white bg-black relative px-5 md:px-20">
       <h2 className="">
-        {category} {formattedContentType}
+        {formattedCategoryName} {formattedContentType}
       </h2>
-      {category}</div>
-  )
-}
+    </div>
+  );
+};
 
 MovieSlider.propTypes = {
   category: PropTypes.string.isRequired,
