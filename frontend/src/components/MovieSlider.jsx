c8e55b03434e7e3a63643e71919b6d27a5e895b5
Mheyrie <omota4fidelity@gmail.com>
2025-02-25 23:24:05 +0100

    scroll added on slider

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,60 +1,89 @@
 import PropTypes from "prop-types";
 import { useContentStore } from "../store/content";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { SMALL_IMG_BASE_URL } from "../utils/constant";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-
 export const MovieSlider = ({ category }) => {
   const { contentType } = useContentStore();
   const [content, setContent] = useState([]);
-  const[showArrows, setShowArrows] = useState(false);
+  const [showArrows, setShowArrows] = useState(false);
+
+  const sliderRef = useRef(null);
 
   console.log("category is here:", category);
 
   const formattedContentType = contentType === "movie" ? "Movies" : "TV Shows";
- const formattedCategoryName = category.replaceAll("_"," ")[0].toUpperCase() + category.replaceAll("_"," ").slice(1);
+  const formattedCategoryName =
+    category.replaceAll("_", " ")[0].toUpperCase() +
+    category.replaceAll("_", " ").slice(1);
 
- useEffect(()=>{
-  const getContent = async()=>{
-    const res = await axios.get(`/api/v1/${contentType}/${category}`);
-    setContent(res.data.content);
-  }
-  getContent()
- }, [category, contentType]);
+  useEffect(() => {
+    const getContent = async () => {
+      const res = await axios.get(`/api/v1/${contentType}/${category}`);
+      setContent(res.data.content);
+    };
+    getContent();
+  }, [category, contentType]);
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const scrollRight = () => {
+    sliderRef.current.scrollBy({
+      left: sliderRef.current.offsetWidth,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="text-white bg-black relative px-5 md:px-20"
-    onMouseEnter={()=>setShowArrows(true)}
-    onMouseLeave={()=>setShowArrows(false)}
+    <div
+      className="text-white bg-black relative px-5 md:px-20"
+      onMouseEnter={() => setShowArrows(true)}
+      onMouseLeave={() => setShowArrows(false)}
     >
       <h2 className="text-2xl font-bold mb-2">
         {formattedCategoryName} {formattedContentType}
       </h2>
-      <div className="flex space-x-4 overflow-x-scroll">
-        {content.map((item)=>(
-          <Link to={`/watch/${item.id}`} className="min-w-[250px] relative group" key={item.id}>
+      <div className="flex space-x-4 overflow-x-scroll" ref={sliderRef}>
+        {content.map((item) => (
+          <Link
+            to={`/watch/${item.id}`}
+            className="min-w-[250px] relative group"
+            key={item.id}
+          >
             <div className="rounded-lg overflow-hidden">
-              <img src={SMALL_IMG_BASE_URL+item.backdrop_path} alt=""
-              className="transition-transform duration-300 ease-in-out group-hover:scale-125"
+              <img
+                src={SMALL_IMG_BASE_URL + item.backdrop_path}
+                alt=""
+                className="transition-transform duration-300 ease-in-out group-hover:scale-125"
               />
             </div>
-            <p className="mt-2 text-center">
-              {item.title || item.name}
-            </p>
+            <p className="mt-2 text-center">{item.title || item.name}</p>
           </Link>
-        )
-        )}
+        ))}
       </div>
-      {showArrows &&(
+      {showArrows && (
         <>
-        <button className="absolute top-1/2 -translate-y-1/2 left-5 md:left-20 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75text-white z-10">
-<ChevronLeft size={24} />
-        </button>
-        <button className="absolute top-1/2 -translate-y-1/2 right-5 md:right-20 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10">
-<ChevronRight size={24} />
-        </button>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 left-5 md:left-20 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75text-white z-10"
+            onClick={scrollLeft}
+          >
+            <ChevronLeft size={24} />
+          </button>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 right-5 md:right-20 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+            onClick={scrollRight}
+          >
+            <ChevronRight size={24} />
+          </button>
         </>
       )}
     </div>
