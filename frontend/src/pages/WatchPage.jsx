524d773dd67b9b65944c449f35be686958691aa1
Mheyrie <omota4fidelity@gmail.com>
2025-02-27 22:08:16 +0100

    updated

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function WatchPage() {
   const { id } = useParams();
@@ -85,11 +85,11 @@ export default function WatchPage() {
             </button>
             <button
               className={`bg-gray-500/70 text-white hover:bg-gray-500 py-2 px-4 rounded${
-                currentTrailerIds === 0 ? "cursor-not-allowed opacity-50" : ""
+                currentTrailerIds === trailers.length-1 ? "cursor-not-allowed opacity-50" : ""
               }`}
-              disabled={currentTrailerIds === 0}
+              disabled={currentTrailerIds === trailers.length-1}
             >
-              <ChevronLeft size={24} />
+              <ChevronRight size={24} />
             </button>
           </div>
         )}
