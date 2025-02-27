c0a1a0280f5ce0d938486002e07f0a930ddee15f
Mheyrie <omota4fidelity@gmail.com>
2025-02-27 21:46:54 +0100

    updating

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {useContentStore} from "../store/ContentStore";
+import { useContentStore } from "../store/ContentStore";
+import axios from "axios";
 
 export default function WatchPage() {
   const { id } = useParams();
@@ -8,6 +9,24 @@ export default function WatchPage() {
   const [currentTrailerIds, setCurrentTrailerIds] = useState(0);
   const [loading, setLoading] = useState(true);
   const [content, setContent] = useState(null);
-  const {contentType}=useContentStore() 
+  const [similarContent, setSimilarContent] = useState([]);
+  const { contentType } = useContentStore();
+
+  useEffect(() => {
+    const getTrailers = async () => {
+      try {
+        const response = await axios.get(
+          `/api/v1/${contentType}/${id}/trailers`
+        );
+        setTrailers(response.data.trailers);
+      } catch (error) {
+        if (error.message.includes("404")) {
+          setTrailers([]);
+        }
+      }
+    };
+    getTrailers();
+  }, [id, contentType]);
+
   return <div>WatchPage</div>;
 }
