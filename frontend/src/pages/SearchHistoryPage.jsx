9b4200cffb4cd06f6a1871db753754ff90c11d60
Mheyrie <omota4fidelity@gmail.com>
2025-03-01 20:52:03 +0100

    updated

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { use } from "react";
+import { useState } from "react";
+
 export default function SearchHistoryPage() {
+  const [searchHistory, setSearchHistory] = useState([]);
+  useEffect(() => {
+     const getSearchHistory = async () => {
+      try{
+const res = await axios.get(`/api/v1/search-history`);
+setSearchHistory(res.data.history); 
+      }catch(error){
+        console.log(error);
+        setSearchHistory([]);
+      }};
+      getSearchHistory();
+  }, []);
   return (
     <div>SearchHistoryPage</div>
   )
