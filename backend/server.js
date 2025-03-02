2c4a1b1ccd1a06cec28a2f275211be7bd6b7bfc1
Mheyrie <omota4fidelity@gmail.com>
2025-03-02 14:02:54 +0100

    deploying settings

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //Es Module
 // const express = require('express') - common js
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -15,6 +16,7 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); //Allow use to use/parse req.body
 app.use(cookieParser())
@@ -23,6 +25,14 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if(ENV_VARS.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
